78609c1192feeabbeb28b7d7a8b0e04e410de15d
Benny Sufrin <bennysuf@gmail.com>
2023-01-01 20:25:46 -0500

    commit tree 9a9c01738880a3a1e1d3b6cc1b730291b80a5b5a

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,27 @@
+require 'pry'
+class Book
+    attr_reader :title, :author, :page_count, :genre
+    attr_writer :page_count
 
+    def initialize(title="")
+        @title = title
+    end
+    def author=(author)
+    @author = author
+    end
+    def page_count=(page)
+    @page_count = page
+    end
+    def genre=(genre)
+    @genre = genre
+    end
 
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+end
+
+book = Book.new("And Then There Were None")
+
+# binding.pry
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,34 @@
 # Make your shoe class here!
+require 'pry'
+
+class Shoe
+    attr_reader :brand, :color, :size, :material, :condition
+    attr_writer :condition
+
+    def initialize(brand)
+        @brand = brand
+    end
+    def color=(color)
+        @color = color
+        end
+    def size=(size)
+        @size = size
+    end
+    def material=(material)
+        @material = material
+    end
+    def condition=(condition)
+        @condition = condition
+    end
+    def cobble
+        if condition != "new"
+            @condition = "new"
+            puts "Your shoe is as good as new!"
+        end
+    end
+
+end
+
+# shoe = Shoe.new
+
+# binding.pry
